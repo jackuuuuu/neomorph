b632fad650786a6fa3b5aa7580c357a89e8aee68
jackuuuuu <58741405+jackuuuuu@users.noreply.github.com>
2020-01-25 12:40:50 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,20 @@
+function dive1() {
+    document.getElementById("diver1").style.boxShadow = "0px 0px 0px #00077e";
+    document.getElementById("diver1").style.background = "#00077e";
+    document.getElementById("diver1").style.color = "#00077e";
+    document.getElementById("diver1").style.transition = "2s";
+
+}
+
+function dive2() {
+    document.getElementById("diver2").style.opacity = "0";
+    document.getElementById("diver2").style.transition = "2s";
+}
+
+function dive3() {
+    document.getElementById("diver3").style.boxShadow = "0px 0px 0px #00077e";
+    document.getElementById("diver3").style.background = "#00077e";
+    document.getElementById("diver3").style.color = "#00077e";
+    document.getElementById("diver3").style.transition = "2s";
+
+}
