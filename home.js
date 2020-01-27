cefac2701a5d3e5c4b4695fabde83e33c933a67a
jackuuuuu <58741405+jackuuuuu@users.noreply.github.com>
2020-01-27 13:16:19 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -1,20 +1,76 @@
+
+function diveIMG1() {
+    document.getElementById("diveIMG1").style.opacity = "0";
+    document.getElementById("diveIMG1").style.transition = "2s";
+}
+
+
+function diveIMG2() {
+    document.getElementById("diveIMG2").style.opacity = "0";
+    document.getElementById("diveIMG2").style.transition = "2s";
+}
+
+
 function dive1() {
     document.getElementById("diver1").style.boxShadow = "0px 0px 0px #00077e";
-    document.getElementById("diver1").style.background = "#00077e";
-    document.getElementById("diver1").style.color = "#00077e";
+    document.getElementById("diver1").style.background = "#0438B6";
+    document.getElementById("diver1").style.color = "#0438B6";
     document.getElementById("diver1").style.transition = "2s";
-
 }
 
 function dive2() {
-    document.getElementById("diver2").style.opacity = "0";
+    document.getElementById("diver2").style.color = "#0438B6";
     document.getElementById("diver2").style.transition = "2s";
 }
 
 function dive3() {
-    document.getElementById("diver3").style.boxShadow = "0px 0px 0px #00077e";
-    document.getElementById("diver3").style.background = "#00077e";
-    document.getElementById("diver3").style.color = "#00077e";
+    document.getElementById("diver3").style.boxShadow = "0px 0px 0px #0438B6";
+    document.getElementById("diver3").style.background = "#0438B6";
+    document.getElementById("diver3").style.color = "#0438B6";
     document.getElementById("diver3").style.transition = "2s";
+}
+
+function dive4() {
+    document.getElementById("diver4").style.boxShadow = "0px 0px 0px #0438B6";
+    document.getElementById("diver4").style.background = "#0438B6";
+    document.getElementById("diver4").style.color = "#0438B6";
+    document.getElementById("diver4").style.transition = "2s";
+}
+
+function dive5() {
+    document.getElementById("diver5").style.boxShadow = "0px 0px 0px #0438B6";
+    document.getElementById("diver5").style.background = "#0438B6";
+    document.getElementById("diver5").style.color = "#0438B6";
+    document.getElementById("diver5").style.borderBottomColor = "#0438B6";
+    document.getElementById("diver5").style.transition = "2s";
+}
+
+function dive6() {
+    document.getElementById("diver6").style.boxShadow = "0px 0px 0px #0438B6";
+    document.getElementById("diver6").style.background = "#0438B6";
+    document.getElementById("diver6").style.color = "#0438B6";
+    document.getElementById("diver6").style.transition = "2s";
+}
+
+function dive7() {
+    document.getElementById("diver7").style.boxShadow = "0px 0px 0px #0438B6";
+    document.getElementById("diver7").style.background = "#0438B6";
+    document.getElementById("diver7").style.color = "#0438B6";
+    document.getElementById("diver7").style.transition = "2s";
+}
+
+function bringBack() {
+    document.getElementById("bringB").style.opacity = "100";
+    document.getElementById("bringB").style.transition = "2s";
+    document.getElementById("button1").style.opacity = "0";
+    document.getElementById("button1").style.transition = "2s";
 
 }
+
+
+var audio = new Audio('./chookedi.mp3');
+
+function chookedi() {    
+    audio.play();    
+}
+
