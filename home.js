b4a0ad17d06081de563a984ccf71683750434b53
jackuuuuu <58741405+jackuuuuu@users.noreply.github.com>
2020-01-28 18:33:45 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -1,74 +1,39 @@
 
-function diveIMG1() {
-    document.getElementById("diveIMG1").style.opacity = "0";
-    document.getElementById("diveIMG1").style.transition = "2s";
+function dive() {
+    const list = document.getElementsByClassName("diverComponent");
+    [...list].forEach((element) => {
+        console.log(element);
+        element.style.boxShadow = "0px 0px 0px #00077e";
+        element.style.background = "#0438B6";
+        element.style.color = "#0438B6";
+        element.style.transitionDuration = "2s";
+    })
+    const imglist = document.getElementsByClassName("diveIMG");
+    [...imglist].forEach((element) => {
+        console.log(element);
+        element.style.opacity = "0";
+        element.style.transitionDuration = "2s";
+    })
+        const inputs = document.getElementsByClassName("input");
+    [...inputs].forEach((element) => {
+        console.log(element);
+        element.style.borderBottomColor = "#0438B6";
+        element.style.background = "#0438B6";
+        element.style.placeholder = "#0438B6";
+        element.style.transitionDuration = "2s";
+    })  
+    //     const bringBack = document.getElementsByClassName("bringBack");
+    // [...bringBack].forEach((element) => {
+    //     console.log(element);
+    //     element.style.transition = "2s";
+    //     element.style.opacity = "0";
+    //     element.style.transition = "2s";
+    // })   
 }
 
 
-function diveIMG2() {
-    document.getElementById("diveIMG2").style.opacity = "0";
-    document.getElementById("diveIMG2").style.transition = "2s";
-}
 
-
-function dive1() {
-    document.getElementById("diver1").style.boxShadow = "0px 0px 0px #00077e";
-    document.getElementById("diver1").style.background = "#0438B6";
-    document.getElementById("diver1").style.color = "#0438B6";
-    document.getElementById("diver1").style.transition = "2s";
-}
-
-function dive2() {
-    document.getElementById("diver2").style.color = "#0438B6";
-    document.getElementById("diver2").style.transition = "2s";
-}
-
-function dive3() {
-    document.getElementById("diver3").style.boxShadow = "0px 0px 0px #0438B6";
-    document.getElementById("diver3").style.background = "#0438B6";
-    document.getElementById("diver3").style.color = "#0438B6";
-    document.getElementById("diver3").style.transition = "2s";
-}
-
-function dive4() {
-    document.getElementById("diver4").style.boxShadow = "0px 0px 0px #0438B6";
-    document.getElementById("diver4").style.background = "#0438B6";
-    document.getElementById("diver4").style.color = "#0438B6";
-    document.getElementById("diver4").style.transition = "2s";
-}
-
-function dive5() {
-    document.getElementById("diver5").style.boxShadow = "0px 0px 0px #0438B6";
-    document.getElementById("diver5").style.background = "#0438B6";
-    document.getElementById("diver5").style.color = "#0438B6";
-    document.getElementById("diver5").style.borderBottomColor = "#0438B6";
-    document.getElementById("diver5").style.transition = "2s";
-}
-
-function dive6() {
-    document.getElementById("diver6").style.boxShadow = "0px 0px 0px #0438B6";
-    document.getElementById("diver6").style.background = "#0438B6";
-    document.getElementById("diver6").style.color = "#0438B6";
-    document.getElementById("diver6").style.transition = "2s";
-}
-
-function dive7() {
-    document.getElementById("diver7").style.boxShadow = "0px 0px 0px #0438B6";
-    document.getElementById("diver7").style.background = "#0438B6";
-    document.getElementById("diver7").style.color = "#0438B6";
-    document.getElementById("diver7").style.transition = "2s";
-}
-
-function bringBack() {
-    document.getElementById("bringB").style.opacity = "100";
-    document.getElementById("bringB").style.transition = "2s";
-    document.getElementById("button1").style.opacity = "0";
-    document.getElementById("button1").style.transition = "2s";
-
-}
-
-
-var audio = new Audio('./chookedi.mp3');
+const audio = new Audio('./chookedi.mp3');
 
 function chookedi() {    
     audio.play();    
